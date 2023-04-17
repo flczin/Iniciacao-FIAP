843d1394712f2cfc05824f8fe63ea598d234dfbc
Lucas Raoni <lucasraoniha@gmail.com>
2023-04-17 19:56:07 -0300

    Added comments to some steps in the code

--- main.py
+++ main.py
@@ -17,12 +17,29 @@ while True:
     if landmark_points:
         landmarks = landmark_points[0].landmark
         for id, landmark in enumerate(landmarks[474:478]):
+
+            # this make the points around the eyes
+            # ex: so if you change the numbers to fixed amound
+            # it will make green fixed dots in the camera image 
             x = int(landmark.x * frame_w)
             y = int(landmark.y * frame_h)
+
+            # circle around the retina
+            # frame: the window where the cv2 will draw. So its the webcam screen
+            # (x, y): the coordnates of the circle where it should draw
+            # 3: size of the circles in the draw
+            # last one is color
             cv2.circle(frame, (x, y), 3, (0, 255, 0))
+
             if id == 1:
+
+                # calculations to match the eye location to the current 
+                # screen in your computer
+                # calc: cv2 camera height or width * landmark(???)
                 screen_x = screen_w * landmark.x
                 screen_y = screen_h * landmark.y
+
+                # move the cursor to the calculated x and y
                 pyautogui.moveTo(screen_x, screen_y)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
